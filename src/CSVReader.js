504903d288bd7f8c09b2fb4d265c55526e9aa292
Arjun <76874556+1407arjun@users.noreply.github.com>
2021-10-05 11:43:26 +0530

    Add processing functionality

--- src/CSVReader.js
+++ src/CSVReader.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 function CSVReader() {
     const [file, setFile] = useState();
+    const [csvArray, setCSVArray] = useState([]);
 
     function submitFile() {
         const csvFile = file;
@@ -10,9 +11,27 @@ function CSVReader() {
         reader.onload = function(ev) {
             const data = ev.target.result;
             console.log(data);
+            processCSVData(data);
         }
 
         reader.readAsText(csvFile);
+    }
+
+    //Comma as the deliminator
+    function processCSVData(data) {
+        const headers = data.slice(0, data.indexOf("\n").split(","));
+        const rows = data.slice(data.indexOf("\n") + 1).split("\n");
+
+        const array = rows.map((row) => {
+            const values = row.split(",");
+            const object = headers.reduce((obj, header, i) => {
+                obj[header] = values[i];
+                return obj;
+            }, {});
+            return object;
+        });
+
+        setCSVArray(array);
     }
 
     return(
