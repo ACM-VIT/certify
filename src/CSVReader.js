6e906e7902acf11d8094c26beb74fefec0b72122
Arjun <76874556+1407arjun@users.noreply.github.com>
2021-10-05 11:35:36 +0530

    Add reader, functionality to the form

--- src/CSVReader.js
+++ src/CSVReader.js
@@ -3,12 +3,32 @@ import { useState } from 'react';
 function CSVReader() {
     const [file, setFile] = useState();
 
+    function submitFile() {
+        const csvFile = file;
+        const reader = new FileReader();
+
+        reader.onload = function(ev) {
+            const data = ev.target.result;
+            console.log(data);
+        }
+
+        reader.readAsText(csvFile);
+    }
+
     return(
         <form id='submit-csv-form'>
-            <input type="file" accept=".csv" id="file">
+            <input type="file" accept=".csv" id="file"
+                onChange={(ev) => {
+                    setFile(ev.target.files[0])
+                }}>
             </input>
             <br/>
-            <button>Submit</button>
+            <button
+                onClick={(ev) => {
+                    ev.preventDefault();
+                    if (file)
+                        submitFile();
+                }}>Submit</button>
         </form>
     );
 }
