5adadd8cad296221c7fd360809e964bde6a3e9de
pranav desai <74852751+pranavvdesai@users.noreply.github.com>
2021-10-09 11:18:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <p class="bg-red-500">Something is here</p>
-
+       <CSVReader csvData={getCSVData}/>
       <Radiob/>
     </div>
 
